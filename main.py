d04ac0e7db9bed7344e246341814450d572d57ff
sinfulcritic <theis@via.dk>
2021-01-12 16:53:07 +0100

    Started basic GUI, GUI non functional

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from count import *
+from tkinter import *
 
-txt_count('test.txt')
+root = Tk()
+
+e = Entry(root)
+e.pack()
+e.focus_set()
+
+init_count = Button(root, text="Start", command=txt_count('test.txt'))
+init_count.pack()
+
+root.mainloop()
