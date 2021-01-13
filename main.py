a7cc549744cc0fe59bb459826166ae7f11298367
sinfulcritic <theis@via.dk>
2021-01-13 09:14:05 +0100

    Added browse button

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from tkinter import *
 root = Tk()
 
 e = Entry(root)
-e.pack()
+e.grid(row=4, column=0 )
 e.focus_set()
 
+
+file_pick = Button(root, text="Browse")
 init_count = Button(root, text="Start", command=txt_count('test.txt'))
-init_count.pack()
+file_pick.grid(row=1, column=0)
+init_count.grid(row=2, column=0)
 
 root.mainloop()
